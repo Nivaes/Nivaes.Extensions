33efc86c3fd33f383f6af9dd556b58e75e94ca89
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-02-28 13:01:56 +0100

    Include netstandard2.0

--- Nivaes.Async/Sources/AsyncLazy.cs
+++ Nivaes.Async/Sources/AsyncLazy.cs
@@ -7,27 +7,22 @@
 
     public class AsyncLazy<T> : Lazy<ValueTask<T>>
     {
-        [DebuggerHidden]
         public AsyncLazy()
             : base()
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(bool isThreadSafe)
             : base(isThreadSafe)
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(T valueFactory)
             : base(() => new ValueTask<T>(valueFactory))
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(Func<T> valueFactory)
             : base(() => new ValueTask<T>(valueFactory.Invoke()))
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(Func<Task<T>> taskFactory)
             : base(() => new ValueTask<T>(taskFactory.Invoke()))
         { }
@@ -37,33 +32,26 @@
             : base(mode)
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(T valueFactory, bool isThreadSafe)
             : base(() => new ValueTask<T>(valueFactory), isThreadSafe)
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(Func<T> valueFactory, bool isThreadSafe)
             : base(() => new ValueTask<T>(Task.Factory.StartNew(valueFactory)), isThreadSafe)
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(Func<Task<T>> taskFactory, bool isThreadSafe)
             : base(() => new ValueTask<T>(taskFactory.Invoke()), isThreadSafe)
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(T valueFactory, LazyThreadSafetyMode mode)
             : base(() => new ValueTask<T>(valueFactory), mode)
         { }
 
-        [DebuggerHidden]
-        [DebuggerStepperBoundary]
         public AsyncLazy(Func<T> valueFactory, LazyThreadSafetyMode mode)
             : base(() => new ValueTask<T>(Task.Factory.StartNew(valueFactory)), mode)
         { }
 
-        [DebuggerHidden]
         public AsyncLazy(Func<Task<T>> taskFactory, LazyThreadSafetyMode mode)
             : base(() => new ValueTask<T>(taskFactory.Invoke()), mode)
         { }
