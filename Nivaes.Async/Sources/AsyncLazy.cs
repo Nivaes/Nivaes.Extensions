9253f1baf27f09d6d465d692e3194de512433029
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2020-07-08 12:51:13 +0200

    Include ValueTask with parameter in AsyncLazy.

--- Nivaes.Async/Sources/AsyncLazy.cs
+++ Nivaes.Async/Sources/AsyncLazy.cs
@@ -23,6 +23,10 @@
             : base(() => new ValueTask<T>(valueFactory.Invoke()))
         { }
 
+        public AsyncLazy(Func<ValueTask<T>> taskFactory)
+           : base(() => taskFactory.Invoke())
+        { }
+
         public AsyncLazy(Func<Task<T>> taskFactory)
             : base(() => new ValueTask<T>(taskFactory.Invoke()))
         { }
@@ -40,6 +44,10 @@
             : base(() => new ValueTask<T>(Task.Factory.StartNew(valueFactory)), isThreadSafe)
         { }
 
+        public AsyncLazy(Func<ValueTask<T>> taskFactory, bool isThreadSafe)
+            : base(() => taskFactory.Invoke(), isThreadSafe)
+        { }
+
         public AsyncLazy(Func<Task<T>> taskFactory, bool isThreadSafe)
             : base(() => new ValueTask<T>(taskFactory.Invoke()), isThreadSafe)
         { }
@@ -50,6 +58,10 @@
 
         public AsyncLazy(Func<T> valueFactory, LazyThreadSafetyMode mode)
             : base(() => new ValueTask<T>(Task.Factory.StartNew(valueFactory)), mode)
+        { }
+
+        public AsyncLazy(Func<ValueTask<T>> taskFactory, LazyThreadSafetyMode mode)
+           : base(() => taskFactory.Invoke(), mode)
         { }
 
         public AsyncLazy(Func<Task<T>> taskFactory, LazyThreadSafetyMode mode)
