17a8999b6204f28c87d205b66a2e72a2b5613482
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-03-31 11:49:23 +0200

    Include enumerator helper.

---
+++ Nivaes.Async/Sources/EnumeratorHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Nivaes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class EnumeratorHelper
+    {
+        public static IEnumerable<T> Take<T>(this IEnumerator<T> enumerator, int n)
+        {
+            ArgumentNullException.ThrowIfNull(enumerator);
+
+            int i = 0;
+            while (i++ < n && enumerator.MoveNext())
+            {
+                yield return enumerator.Current;
+            }
+        }
+    }
+}
