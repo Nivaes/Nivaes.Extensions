2365dadcc5e908d1f253ee0c418ce593f66cacc4
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-03-31 12:39:50 +0200

    Change name of directorys.

---
+++ Nivaes.Tools.UnitTest/Tests/Guid/GuidHelperTest.cs
@@ -0,0 +1,58 @@
+﻿namespace Nivaes.Tools.UnitTest.Tests.Guid
+{
+    using System;
+    using Nivaes.Sources.Guid;
+    using Xunit;
+
+    [Trait("TestType", "Unit")]
+    public class GuidHelperTest
+    {
+        [Fact]
+        public void GuidHelperTest01()
+        {
+            var guid1 = Guid.NewGuid();
+            var guid2 = Guid.NewGuid();
+
+            var combineGuid1 = GuidHelper.Combine(guid1, guid2);
+            var combineGuid2 = GuidHelper.Combine(guid2, guid1);
+
+            Assert.Equal(combineGuid1, combineGuid2);
+        }
+
+        [Fact]
+        public void GuidHelperTest02()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var guid1 = Guid.NewGuid();
+                var guid2 = Guid.NewGuid();
+
+                var combineGuid1 = GuidHelper.Combine(guid1, guid2);
+                var combineGuid2 = GuidHelper.Combine(guid2, guid1);
+
+                Assert.Equal(combineGuid1, combineGuid2);
+            }
+        }
+
+        [Fact]
+        public void GuidHelperTest03()
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                var guid1 = Guid.NewGuid();
+                var guid2 = Guid.NewGuid();
+                var guid3 = Guid.NewGuid();
+                var guid4 = Guid.NewGuid();
+                var guid5 = Guid.NewGuid();
+                var guid6 = Guid.NewGuid();
+                var guid7 = Guid.NewGuid();
+                var guid8 = Guid.NewGuid();
+
+                var combineGuid1 = GuidHelper.Combine(guid1, guid2, guid3, guid4, guid5, guid6, guid7, guid8);
+                var combineGuid2 = GuidHelper.Combine(guid2, guid5, guid6, guid8, guid7, guid1, guid3, guid4);
+
+                Assert.Equal(combineGuid1, combineGuid2);
+            }
+        }
+    }
+}
---
+++ Nivaes.Tools/Sources/Guid/GuidHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Nivaes.Sources.Guid
+{
+    using System;
+
+    public static class GuidHelper
+    {
+        public static Guid Combine(params Guid[] guids)
+        {
+            ArgumentNullException.ThrowIfNull(guids);
+
+            byte[] buffer = new byte[16];
+
+            foreach (var guid in guids)
+            {
+                var guidBuffer = guid.ToByteArray();
+                for (int i = 0; i < 16; i++)
+                {
+                    buffer[i] = (byte)((buffer[i] + guidBuffer[i]) % 256);
+                }
+            }
+
+            return new Guid(buffer);
+        }
+    }
+}
