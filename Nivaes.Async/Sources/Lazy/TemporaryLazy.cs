84f6d9a07610287bb596ff2523604b8e24601fcd
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-03-31 11:57:18 +0200

    Include TemporaryLazy not asysnc.

---
+++ Nivaes.Async/Sources/Lazy/TemporaryLazy.cs
@@ -0,0 +1,50 @@
+﻿namespace Nivaes
+{
+    using System;
+
+    public class TemporaryLazy<T>
+    {
+        private readonly Func<T> mFactory;
+        private readonly TimeSpan mLifetime;
+        private readonly object mValueLock;
+
+        private T? mValue;
+        private DateTime mCreationTime;
+
+        public TemporaryLazy(Func<T> factory, TimeSpan lifetime)
+        {
+            mFactory = factory;
+            mLifetime = lifetime;
+            mValueLock = new object();
+        }
+
+        public bool HasValue
+        {
+            get
+            {
+                lock (mValueLock)
+                {
+                    return !(Equals(mValue, default(T)) && mCreationTime.Add(mLifetime) > DateTime.UtcNow);
+                }
+            }
+        }
+
+        public T Value
+        {
+            get
+            {
+                lock (mValueLock)
+                {
+                    if (Equals(mValue, default(T)) || mCreationTime.Add(mLifetime) > DateTime.UtcNow)
+                    {
+                        mValue = mFactory();
+                        mCreationTime = DateTime.UtcNow;
+                    }
+
+                    return mValue;
+                }
+            }
+        }
+    }
+}
+}
