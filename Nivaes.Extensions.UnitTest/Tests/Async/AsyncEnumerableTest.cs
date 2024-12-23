9f0ce9ec53276b4b7134f836c526e82ad1ce14f6
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-12-23 18:53:10 +0100

    Copy Async test.

---
+++ Nivaes.Extensions.UnitTest/Tests/Async/AsyncEnumerableTest.cs
@@ -0,0 +1,71 @@
+﻿namespace Nivaes.UnitTest
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Linq;
+    using Xunit;
+    using Xunit.Abstractions;
+    using FluentAssertions;
+
+    [Trait("TestType", "Unit")]
+    public class AsyncEnumerableTest
+    {
+        private readonly ITestOutputHelper mTestOutputHelper;
+
+        public AsyncEnumerableTest(ITestOutputHelper testOutputHelper)
+        {
+            mTestOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task IEnumerableToAsyncEnumerableTest()
+        {
+            IEnumerable<int> values = new List<int>() { 1, 2, 3, 4, 5 };
+
+            var asyncValues = values.ToAsyncEnumerable();
+
+            int i = 0;
+            await foreach (var value in asyncValues)
+            {
+                mTestOutputHelper.WriteLine($"{value}");
+                i++;
+            }
+
+            i.Should().Be(values.Count());
+        }
+
+        [Fact]
+        public async Task ListToAsyncEnumerableTest()
+        {
+            var values = new List<int>() { 1, 2, 3, 4, 5 };
+
+            var asyncValues = values.ToAsyncEnumerable();
+
+            int i = 0;
+            await foreach (var value in asyncValues)
+            {
+                mTestOutputHelper.WriteLine($"{value}");
+                i++;
+            }
+
+            i.Should().Be(values.Count);
+        }
+
+        [Fact]
+        public async Task ArrayToAsyncEnumerableTest()
+        {
+            var values = new int[] { 1, 2, 3, 4, 5 };
+
+            var asyncValues = values.ToAsyncEnumerable();
+
+            int i = 0;
+            await foreach (var value in asyncValues)
+            {
+                mTestOutputHelper.WriteLine($"{value}");
+                i++;
+            }
+
+            i.Should().Be(values.Length);
+        }
+    }
+}
