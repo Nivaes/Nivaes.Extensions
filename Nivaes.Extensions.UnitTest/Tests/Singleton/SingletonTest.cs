9ca0b8fb2af0e275f6b77ba7d0579bde5fdb2686
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2024-12-23 18:52:50 +0100

    Copy  singlenton.

---
+++ Nivaes.Extensions.UnitTest/Tests/Singleton/SingletonTest.cs
@@ -0,0 +1,72 @@
+﻿namespace Nivaes.UnitTest
+{
+    using System;
+    using Xunit;
+
+    [Trait("TestType", "Unit")]
+    public class SingletonTest
+    {
+        public class TestClass1()
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+
+        public class TestClass2()
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+
+        [Fact]
+        public void OneSingletonTest()
+        {
+            var instance1 = Singleton<TestClass1>.Instance;
+            Assert.NotNull(instance1);
+
+            var instance2 = Singleton<TestClass1>.Instance;
+            Assert.NotNull(instance2);
+
+            Assert.Equal(instance1.Id, instance2.Id);
+        }
+
+        [Fact]
+        public void TwoSingletonTest()
+        {
+            var instance1 = Singleton<TestClass1>.Instance;
+            Assert.NotNull(instance1);
+
+            var instance2 = Singleton<TestClass2>.Instance;
+            Assert.NotNull(instance2);
+
+            Assert.NotEqual(instance1.Id, instance2.Id);
+        }
+
+        [Fact]
+        public void MultipleTest()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                var instance1 = Singleton<TestClass1>.Instance;
+                Assert.NotNull(instance1);
+
+                var instance2 = Singleton<TestClass2>.Instance;
+                Assert.NotNull(instance2);
+
+                Assert.NotEqual(instance1.Id, instance2.Id);
+            }
+        }
+
+        [Fact]
+        public void ClearSingletonTest()
+        {
+            var instance1 = Singleton<TestClass1>.Instance;
+            Assert.NotNull(instance1);
+
+            Singleton<TestClass2>.Clear();
+
+            var instance2 = Singleton<TestClass1>.Instance;
+            Assert.NotNull(instance2);
+
+            Assert.NotEqual(instance1.Id, instance2.Id);
+        }
+    }
+}
---
+++ Nivaes.Extensions/Sources/Singleton/Singleton.cs
@@ -0,0 +1,56 @@
+﻿namespace Nivaes
+{
+    using System;
+    using System.Collections.Concurrent;
+
+    public abstract class Singleton
+    {
+        protected Singleton()
+        {
+        }
+
+        protected static readonly ConcurrentDictionary<Type, Singleton> Instances = new();
+
+        public abstract void ClearInstance();
+
+        public static void Clear()
+        {
+            foreach (var instance in Instances)
+            {
+                if (Instances.TryRemove(instance.Key, out Singleton? value))
+                {
+                    value.ClearInstance();
+                }
+            }
+        }
+    }
+
+    public class Singleton<T> : Singleton
+         where T : new()
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2743:Static fields should not be used in generic types", Justification = "<Pending>")]
+        private static object? mInstance;
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Critical Code Smell", "S2696:Instance members should not write to \"static\" fields", Justification = "<Pending>")]
+        public override void ClearInstance()
+        {
+            mInstance = null;
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (mInstance == null)
+                {
+                    Instances.GetOrAdd(typeof(T), (_) =>
+                    {
+                        mInstance = new T();
+                        return new Singleton<T>();
+                    });
+                }
+                return (T)mInstance!;
+            }
+        }
+    }
+}
