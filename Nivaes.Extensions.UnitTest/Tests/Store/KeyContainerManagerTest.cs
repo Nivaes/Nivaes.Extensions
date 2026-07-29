b2cbffec4b4f561e78d8e3c16748111dc95ee09a
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2026-07-29 17:32:50 +0200

    Include KeyContainerManager

---
+++ Nivaes.Extensions.UnitTest/Tests/Store/KeyContainerManagerTest.cs
@@ -0,0 +1,115 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Nivaes.App.UnitTest
+{
+    [Trait("TestType", "Unit")]
+    public class KeyContainerManagerTest
+    {
+        #region TestClass
+        public class TestClass1()
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+
+        public class TestClass2()
+        {
+            public Guid Id { get; } = Guid.NewGuid();
+        }
+        #endregion
+
+        [Fact]
+        public void LoadContainerManagerTest()
+        {
+            var instance1_1 = new TestClass1();
+            var instance2_1 = new TestClass1();
+            int key1 = 56226, key2 = 88951;
+
+            var manager = new KeyContainerManager<TestClass1>(new[]
+            {
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 1232, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 553681, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 8851, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 88442, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 87942, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = key1, Value = instance1_1 },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 615882, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 2233584, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 5584, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = key2, Value = instance2_1},
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 223213622, Value = new TestClass1() },
+            });
+
+            var result1 = manager.TryGetValue(key1, out var instance1_2);
+            result1.ShouldBeTrue();
+
+            instance1_1.ShouldNotBeNull();
+            instance1_2.ShouldNotBeNull();
+            instance1_1.Id.ShouldBe(instance1_2!.Id);
+            instance1_1.ShouldBeSameAs(instance1_2);
+
+            var result2 = manager.TryGetValue(key2, out var instance2_2);
+            result2.ShouldBeTrue();
+
+            instance2_1.ShouldNotBeNull();
+            instance2_2.ShouldNotBeNull();
+            instance2_1.Id.ShouldBe(instance2_2!.Id);
+            instance2_1.ShouldBeSameAs(instance2_2);
+        }
+
+        [Fact]
+        public void MergeContainerManagerTest()
+        {
+            var instance1_1 = new TestClass1();
+            var instance2_1 = new TestClass1();
+            int key1 = 56226, key2 = 88951;
+
+            var manager = new KeyContainerManager<TestClass1>(new[]
+            {
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 1232, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 553681, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 8851, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 88442, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 87942, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = key1, Value = instance1_1 },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 615882, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 2233584, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 5584, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 223213622, Value = new TestClass1() },
+            });
+
+            manager.Merge(new[]
+            {
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 24232, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 912793, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 15217, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 3432, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 1685, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 1345234, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 3242113, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 123423, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = key2, Value = instance2_1},
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 3151233, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 23423425, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 23432, Value = new TestClass1() },
+                new KeyContainerManager<TestClass1>.KeyStoreItem { Key = 2572435, Value = new TestClass1() },
+            });
+
+            var result1 = manager.TryGetValue(key1, out var instance1_2);
+            result1.ShouldBeTrue();
+
+            instance1_1.ShouldNotBeNull();
+            instance1_2.ShouldNotBeNull();
+            instance1_1.Id.ShouldBe(instance1_2!.Id);
+            instance1_1.ShouldBeSameAs(instance1_2);
+
+            var result2 = manager.TryGetValue(key2, out var instance2_2);
+            result2.ShouldBeTrue();
+
+            instance2_1.ShouldNotBeNull();
+            instance2_2.ShouldNotBeNull();
+            instance2_1.Id.ShouldBe(instance2_2!.Id);
+            instance2_1.ShouldBeSameAs(instance2_2);
+        }
+    }
+}
---
+++ Nivaes.Extensions/Sources/Store/KeyContainerManager.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nivaes 
+{
+    public class KeyContainerManager<TValue>
+    {
+        private readonly Lock @lock = new();
+
+        private KeyStoreItem[] mValues;
+
+        public struct KeyStoreItem
+        {
+            public nint Key { get; set; }
+            public TValue Value { get; set; }
+        }
+
+        private sealed class KeyPresentationComparer
+            : IComparer<KeyStoreItem>
+        {
+            public int Compare(KeyStoreItem x, KeyStoreItem y)
+            {
+                return x.Key.CompareTo(y.Key);
+            }
+        }
+
+        public KeyContainerManager()
+        {
+            mValues = [];
+        }
+
+        public KeyContainerManager(KeyStoreItem[] values)
+        {
+            lock (@lock)
+            {
+                mValues = values;
+                var keyInstanceResolverValues = new Span<KeyStoreItem>(mValues);
+                keyInstanceResolverValues.Sort(new KeyPresentationComparer());
+            }
+        }
+
+        public void Merge(KeyStoreItem[] newValues)
+        {
+            lock (@lock)
+            {
+                var keyInstanceResolverValues = new Span<KeyStoreItem>(newValues);
+                keyInstanceResolverValues.Sort(new KeyPresentationComparer());
+
+                var oldValues = mValues;
+                var allValues = new KeyStoreItem[oldValues.Length + newValues.Length];
+                int i = 0, j = 0, m = 0;
+
+                while (i < oldValues.Length && j < newValues.Length)
+                {
+                    if (oldValues[i].Key < newValues[j].Key)
+                    {
+                        allValues[m++] = oldValues[i++];
+                    }
+                    else
+                    {
+                        allValues[m++] = newValues[j++];
+                    }
+                }
+                while (i < oldValues.Length)
+                {
+                    allValues[m++] = oldValues[i++];
+                }
+                while (j < newValues.Length)
+                {
+                    allValues[m++] = newValues[j++];
+                }
+
+                mValues = allValues;
+            }
+        }
+
+        protected internal bool TryGetValue(nint key, [MaybeNullWhen(false)] out TValue presentationType)
+        {
+            lock (@lock)
+            {
+                var result = TryGetValue(key, out int position);
+
+                if (result)
+                {
+                    presentationType = mValues[position].Value;
+                    return true;
+                }
+                else
+                {
+                    presentationType = default;
+                    return false;
+                }
+            }
+        }
+
+        protected bool TryGetValue(nint key, [MaybeNullWhen(false)] out int position)
+        {
+            lock (@lock)
+            {
+                var high = mValues.Length - 1;
+                var low = 0;
+
+                while (low <= high)
+                {
+                    int mid = (high + low) / 2;
+                    var midKey = mValues[mid].Key;
+
+                    if (midKey == key)
+                    {
+                        position = mid;
+                        return true;
+                    }
+                    else
+                    {
+                        if (key < midKey)
+                            high = mid - 1;
+                        else
+                            low = mid + 1;
+                    }
+                }
+                position = -1;
+                return false;
+            }
+        }
+    }
+}
